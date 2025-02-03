7297b7a68c8ec391491e1efaf9b391509a9f7af7
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2025-02-03 15:00:28 +0530

    hide the env properly

--- agent_backend/config/settings.py
+++ agent_backend/config/settings.py
@@ -4,8 +4,8 @@ load_dotenv()
 
 class Settings(BaseSettings):
     MONGO_URI: str
-    HF_MODEL: str = "meta-llama/Meta-Llama-3-8B"
-    HF_API_KEY: str = "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"
+    HF_MODEL: str
+    HF_API_KEY: str
 
     class Config:
         env_file = ".env"
